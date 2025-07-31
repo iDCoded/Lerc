aafa766e28f4c586ac61f7491bf4e8c5489ca531
Dhruv Anand <ananddhruv29@gmail.com>
2025-07-31 23:28:09 +0530

    Add ShadCN Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useMicrophone } from "@/hooks/useMicrophone";
 
 export default function Home() {
@@ -11,17 +12,17 @@ export default function Home() {
 			<main className="flex flex-col items-center gap-4 mt-10">
 				<h1>Lerc Voice Assistant</h1>
 				{!isRecording ? (
-					<button
+					<Button
 						onClick={startRecording}
 						className="px-4 py-2 bg-green-500 text-white rounded-lg">
 						Start Recording
-					</button>
+					</Button>
 				) : (
-					<button
+					<Button
 						onClick={stopRecording}
 						className="px-4 py-2 bg-red-500 text-white rounded-lg">
 						Stop Recording
-					</button>
+					</Button>
 				)}
 
 				{audioUrl && (
